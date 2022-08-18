f5f04bf6d493fbcb62f33cf6b59959858924c476
Joel Manley <j_90@live.co.uk>
2022-08-18 20:40:58 +0100

    adds header and footer files

---
+++ app/public/wp-content/themes/dangertrimz/front-page.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * The front page template file.
+ *
+ * If the user has selected a static page for their homepage, this is what will
+ * appear. 
+ */
+get_header(); ?>
+
+<h1>Test page</h1>
