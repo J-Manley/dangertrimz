a52cc7e80c6bfb4cf7280a808e5ff4c619dc86ed
Joel Manley <j_90@live.co.uk>
2022-08-29 19:07:16 +0100

    modifies header and footer files

--- app/public/wp-content/themes/dangertrimz/front-page.php
+++ app/public/wp-content/themes/dangertrimz/front-page.php
@@ -1,10 +1,9 @@
 <?php
-/*
- * The front page template file.
- *
- * If the user has selected a static page for their homepage, this is what will
- * appear. 
- */
 get_header(); ?>
 
+
 <h1>Test page</h1>
+
+
+
+<?php get_footer(); ?>
--- app/public/wp-content/themes/dangertrimz/header.php
+++ app/public/wp-content/themes/dangertrimz/header.php
@@ -2,9 +2,12 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="profile" href="https://gmpg.org/xfn/11">
+  <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+
+  <?php wp_head(); ?>
 </head>
 
 <body>
